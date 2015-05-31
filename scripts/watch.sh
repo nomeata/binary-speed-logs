6ed67d1c3cc138d281075bd81e3efd365f00dfd1
Joachim Breitner <mail@joachim-breitner.de>
2015-05-31 16:04:03 +0200

    Remove unused scripts

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -7,7 +7,7 @@ cd ~/binary/
 while true
 do
 	(cd binary-master; git pull)
-	for rev in $(cd binary-master; git log --oneline --first-parent 4853c7b467409468ac9029558c8ed3caa4a4be6c..HEAD | cut -d\  -f1 | tac)
+	for rev in $(cd binary-master; git log --format=%H --first-parent 4853c7b467409468ac9029558c8ed3caa4a4be6c..HEAD | tac)
 	do
 		if ! [ -e "$rev.log" -o  -e "$rev.log.broken" -o -d "binary-tmp-$rev" ]
 		then
